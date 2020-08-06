deccc5e7220f40861f3748bf7d91c2e1b6a6d5a0
pranita1519 <impranita19@gmail.com>
2020-08-06 12:21:04 +0530

    second commit 06/08/2020 - 12:20pm , After first commit

---
+++ second.py
@@ -0,0 +1,5 @@
+print("Welcome to second Python program")
+num1=int(input("Enter first number : "))
+num2=int(input("Enter second number : "))
+addition = num1+num2
+print("Addition of two numbers : ",addition)
